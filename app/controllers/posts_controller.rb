809914480b97147da16815054e74235043e17efd
mmaithan <manishmaithani63@gmail.com>
2017-12-26 17:28:19 -0500

    model table changes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = "THis is some sample text for our awesome new Ruby blog"
+		@content_second = "THis is some sample second text for our awesome new Ruby blog"
+	end
+	def new 
+	
+	end
+	def create
+	
+	end
+	def edit
+	
+	end
+
+	def update
+	
+	end
+	def new 
+	
+	end
+	def show
+	
+	end
+	def destroy
+	
+	end	
+
+end
