acb8a567429b7397cc53b253efacdfef57b3f314
mmaithan <manishmaithani63@gmail.com>
2017-12-27 23:34:33 -0500

    Some sommit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,33 @@
 class CategoriesController < ApplicationController
   def index
+	@categories = Category.all
   end
 
   def edit
   end
 
   def new
+	@category = Category.new
+  end
+  
+  def create
+	@category = Category.create(cat_create);
+	redirect_to categories_path
+  end
+  
+  def cat_create
+		params.require(:category).permit(:name)
   end
 
   def show
+	@category = Category.find(params[:id])
+	@title = @category.name
+	@posts = @category.posts
+  end
+  
+  def destroy
+	@category = Category.find(params[:id])
+	@category.destroy
+	redirect_to categories_path
   end
 end
